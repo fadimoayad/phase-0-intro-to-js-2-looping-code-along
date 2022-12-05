5702d76bbf116f0cfc881647d35d3f0d6452e567
fadimoayad <fadimoayad99@yahoo.com>
2022-12-05 23:41:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+let arr=["Guadalupe", "Ollie", "Aki"]
+let surprise="surprise"
+let arr2=[]
+
+function writeCards(arr){
+  for (let i = 0; i <= arr.length-1; i++) {
+     arr2.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`)
+}
+return arr2
+}
+
+function countDown(num){
+
+  while ( num >= 0) {
+     console.log(num)
+     num--;
+  }
+}
+
+
+
